24d5cdaeeef14a6e7e99f2167107b4f7942ec794
Palanath <pala@dusttoash.org>
2023-01-23 14:07:58 -0600

    Add main file

---
+++ src/Ichor.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Ichor.cpp
+// Author      : Palanath
+// Version     :
+// Copyright   : Palanath
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+	srand(time(0));
+
+	cout << rand();
+	return 0;
+}
