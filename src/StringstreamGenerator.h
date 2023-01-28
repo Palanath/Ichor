fe4b9ea395828b87c184fa13df0a01d5edbe11c2
Palanath <pala@dusttoash.org>
2023-01-27 22:24:05 -0600

    Add new generator header

---
+++ src/StringstreamGenerator.h
@@ -0,0 +1,15 @@
+/*
+ * StringstreamGenerator.h
+ *
+ *  Created on: Jan 27, 2023
+ *      Author: Palanath
+ */
+
+#ifndef STRINGSTREAMGENERATOR_H_
+#define STRINGSTREAMGENERATOR_H_
+
+
+
+
+
+#endif /* STRINGSTREAMGENERATOR_H_ */
