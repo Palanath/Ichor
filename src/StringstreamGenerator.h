72691d3248e9453132b96a326e3b49aec059e212
Palanath <pala@dusttoash.org>
2023-01-27 22:24:35 -0600

    Update Stringstream gen

--- src/StringstreamGenerator.h
+++ src/StringstreamGenerator.h
@@ -8,8 +8,53 @@
 #ifndef STRINGSTREAMGENERATOR_H_
 #define STRINGSTREAMGENERATOR_H_
 
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
 
+struct String;
 
+struct Problem {
+	unsigned optionCount;
+	char *question, *code, *footnote, *options[];
+};
 
+/*
+ * malloc()s a char[] and copies the contents of the specified stream to it, then clears the stream.
+ */
+char* flushraw(std::stringstream *stream) {
+	auto size = stream->tellp();
+	char *res = (char*) malloc(size);
+	memcpy(res, stream->str().c_str(), size);
+	stream->str("");
+	return res;
+}
+
+/*
+ * This function appends the null-terminator to the specified stream, malloc()s a new char[], and copies the contents of the specified stream to it, then clears the stream.
+ *
+ * This implementation appends std::ends to the stream (via (*stream)<<std::ends;) and then calls and returns the result of flushraw(stream);.
+ */
+char* flush(std::stringstream *stream) {
+	(*stream) << std::ends;
+	return flushraw(stream);
+}
+
+struct Problem* generateRandomProblem() {
+	int options = rand() % 3 + 3;
+	Problem *problem = (Problem*) malloc(
+			sizeof(struct Problem) + sizeof(char*) * options);
+	problem->optionCount = options;
+
+	// Generate randoms.
+	int firstVar = rand() % 39 - 7, secondVar = rand() % 39 - 7;
+	char firstVarName = rand() % 26 + 'a', secondVarName = rand() % 25 + 'a';
+	if (secondVarName >= firstVarName)
+		secondVarName++;
+
+	return problem;
+}
 
 #endif /* STRINGSTREAMGENERATOR_H_ */
