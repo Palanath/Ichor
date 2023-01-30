b5a9ea30907d6645cd0a4d95e0fc4b55bb56dff6
Palanath <pala@dusttoash.org>
2023-01-29 22:18:04 -0600

    Make (WIP) function for generating distinct random #s

--- src/StringstreamGenerator.h
+++ src/StringstreamGenerator.h
@@ -18,7 +18,7 @@ struct String;
 
 struct Problem {
 	unsigned optionCount;
-	char *question, *code, *footnote, *options[];
+	char *question, *code, *footnote = nullptr, *options[];
 };
 
 /*
@@ -42,17 +42,53 @@ char* flush(std::stringstream *stream) {
 	return flushraw(stream);
 }
 
+/*
+ * Generates count distinct random numbers, within the range specified by min and max, and returns a pointer to an array containing the values.
+ * Note that the random numbers will be returned in sorted order.
+ *
+ * If the range max-min is greater than RAND_MAX, then the results of an invocation of this function are undefined.
+ */
+template<class T> void fillWithDistinctRandoms(unsigned int startpos, unsigned int count, int min,
+		int max, T (*arr)[]) {
+	for (unsigned int i = 0; i < count; ++i) {
+		(*arr)[i] = rand() % (max - min - i) + min;
+
+		bool shifted;
+		for (unsigned int j = 0; j < i; ++j)
+			if ((*arr)[i] >= (*arr)[j]) {
+				++(*arr)[i]; // Shift variable up because it's >= to a value earlier in list.
+				shifted = true;
+			}
+
+		if (shifted)
+			for (unsigned int j = 0; j < i; ++j)
+				if ((*arr)[i] == (*arr)[j]) {
+					++(*arr)[i];
+					j = 0; // Restart and check the array again, as (*arr)[i] might now be conflicting with an earlier value.
+				}
+
+	}
+
+}
+
 struct Problem* generateRandomProblem() {
-	int options = rand() % 3 + 3;
+
+	int optionCount = rand() % 3 + 3;
 	Problem *problem = (Problem*) malloc(
-			sizeof(struct Problem) + sizeof(char*) * options);
-	problem->optionCount = options;
+			sizeof(struct Problem) + sizeof(char*) * optionCount);
+	problem->optionCount = optionCount;
 
 	// Generate randoms.
-	int firstVar = rand() % 39 - 7, secondVar = rand() % 39 - 7;
+	short firstVar = rand() % 39 - 7, secondVar = rand() % 39 - 7;
 	char firstVarName = rand() % 26 + 'a', secondVarName = rand() % 25 + 'a';
 	if (secondVarName >= firstVarName)
 		secondVarName++;
+
+	short options[optionCount] { firstVar * 2 + secondVar };
+	for (unsigned short i = 1; i < optionCount; ++i) {
+		// TODO generate random options and then print them below.
+		options[i] = rand() % 39 - 7;
+	}
 
 	// Calculate sizes.
 	std::stringstream q;
