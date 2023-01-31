6bf97624f0ecf33e00427650f9a0c1c6fca77981
Palanath <pala@dusttoash.org>
2023-01-31 13:30:09 -0600

    Make application interactive

--- src/Ichor.cpp
+++ src/Ichor.cpp
@@ -6,23 +6,63 @@
 // Description : Ichor in C++, Ansi-style
 //============================================================================
 
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 
 #include "Common.hpp"
 #include "generators/StreamGenerator.hpp"
+#include "Utilities.hpp"
 
 int main() {
 	srand(time(0));
 
 	Problem *p = StringGenerator::generateRandomProblem();
 	std::cout << p->question << std::endl << p->code << std::endl << std::endl;
-	for (unsigned i = 0; i < p->optionCount; ++i) {
+	for (unsigned i = 0; i < p->optionCount; ++i)
 		std::cout << '(' << (char) ('A' + i) << ".) " << p->options[i]
 				<< std::endl;
+
+	std::cout << std::endl << "You may use number keys 1-"
+			<< (p->optionCount > 9 ? 9 : p->optionCount)
+			<< " or letter keys to select an answer:" << std::endl;
+
+	unsigned correctAnswerPos = 0;
+
+	int c = getchar();
+	for (; !(c <= '9' && c >= '1') && !(c <= 'z' && c >= 'a') && c != EOF; c =
+			getchar())
+		;
+	if (c <= '9' && c >= '1')
+		c -= '1';
+	else
+		c -= 'a';
+	if (c != EOF) {
+		clearConsole();
+		std::cout << p->question << std::endl << p->code << std::endl
+				<< std::endl;
+		for (unsigned i = 0; i < p->optionCount; ++i) {
+			if (i == correctAnswerPos)
+				std::cout << CONSOLE_GREEN;
+			std::cout << '(' << (char) ('A' + i) << ".) " << p->options[i];
+			if (i == correctAnswerPos)
+				std::cout << CONSOLE_RESET;
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		if (c == correctAnswerPos) {
+			// Correct answer selected.
+			std::cout << "You answered right!";
+		} else {
+			std::cout << "That's not correct...";
+		}
+		std::cout << std::endl;
 	}
 
+	for (unsigned i = 0; i < p->optionCount; ++i)
+		free(p->options[i]);
+	free(p);
 	return 0;
 }
 
