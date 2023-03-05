765bec96e95dd032912319fb2e5c15d65e2025dc
Palanath <pala@dusttoash.org>
2023-03-04 21:03:41 -0600

    Create OptimizationGenerator.cpp

---
+++ src/OptimizationGenerator.cpp
@@ -0,0 +1,15 @@
+/*
+ * OptimizationGenerator.cpp
+ *
+ *  Created on: Mar 5, 2023
+ *      Author: Palanath
+ */
+
+#include "OptimizationGenerator.hpp"
+
+
+
+struct Problem* OptimizationGenerator::generateProblem() {
+	// TODO Add problems
+	return nullptr;
+}
