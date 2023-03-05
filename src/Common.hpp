1e9388be36f829bf27ca4f502bb81fdec0d36be3
Palanath <pala@dusttoash.org>
2023-03-04 20:44:30 -0600

    Add ProblemGenerator class to Common.hpp

--- src/Common.hpp
+++ src/Common.hpp
@@ -15,6 +15,12 @@ struct Problem {
 	char *question, *code, *footnote = nullptr, *options[];
 };
 
+class ProblemGenerator {
+public:
+	virtual Problem* generateProblem() = 0;
+	virtual ~ProblemGenerator() = default;
+};
+
 /*
  * Mallocs a Problem struct that has the specified number of options.
  * The footnote member, which is does not need to be specified, is set to nullptr, and the struct's optionCount member is set to the specified optionCount.
@@ -23,7 +29,7 @@ struct Problem {
  */
 struct Problem* genProblem(unsigned optionCount);
 
-template<typename Printable> void prompt(Printable* output, struct Problem* p) {
+template<typename Printable> void prompt(Printable *output, struct Problem *p) {
 	*output << p->question << std::endl << p->code << std::endl << std::endl;
 	if (p->footnote != nullptr)
 		*output << p->footnote << std::endl;
